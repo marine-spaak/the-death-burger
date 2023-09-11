0d53b700074040450099ea3079fa59200d8d00b3
marine-spaak <marine.spaak@gmail.com>
2023-09-11 11:51:11 +0200

    complements on index and creation of App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Mon app
+      </Text>
+    </View>
+  )
+}
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,16 @@
+// Le fichier app > index.js est le second point d'entrée
+// On pose une SafeAreaView et dedans on va mettre notre composant App.js
+
+import { SafeAreaView } from 'react-native';
+import App from '../App';
+
+const index = () => {
+
+  return (
+    <SafeAreaView>
+      <App />
+    </SafeAreaView>
+  )
+}
+
+export default index;
