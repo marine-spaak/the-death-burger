4d2251fa9c5c11e79bbe9474dacf3619d810500c
marine-spaak <marine.spaak@gmail.com>
2023-09-11 14:28:46 +0200

    ready to start without icons

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Home from './Home/Home';
 
-export {
+export default {
   Home,
 };
--- src/constants/icons.js
+++
@@ -1,6 +0,0 @@
-// Importer ici les icons de la manière suivante
-
-// import name from '../assets/icons/name.png';
-// export default {
-//   name,
-// };
